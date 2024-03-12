386cf6c4d627a9f9cf673e61490afb020f0547ac
wijaemin <minialp2002@naver.com>
2024-03-12 18:54:24 +0900

    fs모듈로 회원가입 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -21,13 +21,11 @@ const process={
      login : async (req,res) => {
         const user = new User(req.body);
         const response = await user.login();
-
         return res.json(response);
     },
-    register : (req,res) =>{
+    register :async (req,res) =>{
         const user = new User(req.body);
-        const response = user.register();
-
+        const response =await  user.register();
         return res.json(response);
     }
 };
