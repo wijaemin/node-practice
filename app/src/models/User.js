2efe76d527cfe8f77260248c311ebddfd9ff6858
wijaemin <minialp2002@naver.com>
2024-03-13 17:28:02 +0900

    AWS RDS와 연동 & dotenv로 환경변수 등록

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -8,15 +8,19 @@ class User {
         this.body =body;
     }
     async login(){
-        const client =this.body;
-        const {id, password} = await UserStorage.getUserInfo(client.id);
-        if(id){
-            if(id === client.id && password === client.password){
-                return {success: true};
+        const client = this.body;
+        try{
+            const users = await UserStorage.getUserInfo(client.id);
+            if(users.id){
+                if(users.id === client.id && users.password === client.password){
+                    return {success: true};
+                }
+                return {success: false , msg :"비밀번호가 틀렸습니다"};
             }
-            return {success: false , msg :"비밀번호가 틀렸습니다"};
+            return {success: false , msg : "아이디가 존재하지 않습니다"};
+        } catch(err){
+            return {success: false , msg : err}
         }
-        return {success: false , msg : "아이디가 존재하지 않습니다"};
     }
     async register(){
         const client =this.body;
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,65 +1,38 @@
 "use strict";
 
-const fs = require("fs").promises;
+const db= require("../config/db");
 
 class UserStorage {
-    static #getUserInfo(data,id){
-        const users =JSON.parse(data);
-        const index =users.id.indexOf(id);//가져온 id가 users에서 몇번째 id인지 가져옴
-        const usersKeys= Object.keys(users);//users의 키값들만 가져옴
-        const userInfo =usersKeys.reduce((newUser, info)=>{//키값들로 반복문 돌림 newUser는 새 오브젝트, info는 키값들 순서대로
-            newUser[info] = users[info][index];
-            return newUser;
-        },{});
-        return userInfo;
-    }
-
-    static #getUsers(data,isAll,fields){
-        const users =JSON.parse(data);
-        if(isAll){
-            return users;
-        }
-        const newUsers= fields.reduce((newUsers,field)=>{
-            if(users.hasOwnProperty(field)){
-                newUsers[field] =users[field];
-            }
-            return newUsers;
-        },{});
-        return newUsers;
-    }
-
-    static getUsers(isAll,...fields){
-        return fs.readFile("./src/databases/users.json")
-        .then((data)=>{
-            return this.#getUsers(data,isAll,fields);
-        })
-        .catch(console.error);
-
-
-    }
     
     static getUserInfo(id){  
-
-        return fs.readFile("./src/databases/users.json")
-        .then((data)=>{
-            return this.#getUserInfo(data,id);
-        })
-        .catch(console.error);
-
+        return new Promise((resolve,reject)=>{
+            const query ="select * from users where id = ?";  
+            db.query(query,[id], (err,data)=>{
+               if(err){
+                reject(`${err}`);
+               }
+               else{
+                resolve(data[0]);
+               }
+            });
+        });
     }
 
-
-
     static async save(userInfo){
-        const users= await this.getUsers(true);
-        if(users.id.includes(userInfo.id)){
-            throw "이미 존재하는 아이디입니다";
-        }
-        users.id.push(userInfo.id);
-        users.name.push(userInfo.name);
-        users.password.push(userInfo.password);
-        fs.writeFile("./src/databases/users.json",JSON.stringify(users));
-        return {success:true};
+        return new Promise((resolve,reject)=>{
+            const query ="insert into users(id, name, password) values(?, ?, ?)";  
+            db.query(
+                query, 
+                [userInfo.id, userInfo.name, userInfo.password], 
+                (err)=>{
+               if(err){
+                reject(`${err}`);
+               }
+               else{
+                resolve({success:true});
+               }
+            });
+        });
     }
 }
 module.exports = UserStorage;
