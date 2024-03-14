82b8896a445431ff33d8bd1511f4cd1ef401bf0f
wijaemin <minialp2002@naver.com>
2024-03-14 22:52:39 +0900

    www.js 수정

--- app/bin/www.js
+++ app/bin/www.js
@@ -1,7 +1,8 @@
-const PORT= process.env.PORT || 3000;
+"use strict";
 
 const logger= require("../src/config/logger");
 const  app =require("../app");
+const PORT= process.env.PORT;
 app.listen(PORT,()=>{
     logger.info(`${PORT} 서버에서 가동되었습니다.`);
 });
